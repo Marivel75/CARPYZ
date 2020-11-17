8ab8c2ab219a89a1a0438fdf4862c98f6d09ff9d
Mikaël <rocavel.dev@zoho.eu>
2020-11-18 00:42:14 +0100

    once again

--- presentation/js/prezCarpyz.js
+++ presentation/js/prezCarpyz.js
@@ -1,15 +1,46 @@
-let positions = ['#pos-1', '#pos-2', '#pos-3', '#pos-4', '#pos-5', '#pos-6', '#pos-7', '#pos-8', '#pos-9', '#pos-10', '#pos-11', '#pos-12', '#pos-13', '#pos-14']
+let positionsY = [848, 1947, 3262, 4448, 5370, 6408, 7210, 8280, 9651, 10537, 11626, 12686, 13412]
 
-let scrollBtn = document.getElementsByClassName('scrollBtn')
+let scrollDownBtns = Array.from(document.querySelectorAll("button[data-title='Go down']"));
+let scrollUpBtns = Array.from(document.querySelectorAll("button[data-title='Go up']"));
 
-
-for (let btn of scrollBtn) {
+for (let btn of scrollDownBtns) {
+  let index = scrollDownBtns.indexOf(btn);
   btn.addEventListener('click', () => {
-    let action = btn.parentNode.attributes[0].textContent;
-    for (let pos of positions) {
-      if (action === pos) {
-        console.log(positions.indexOf(pos) + 1);
-      }
-    }
+    window.scrollTo(0, positionsY[index]);
   });
 };
+
+for (let btn of scrollUpBtns) {
+  let index = scrollUpBtns.indexOf(btn) - 1;
+  btn.addEventListener('click', () => {
+    window.scrollTo(0, positionsY[index]);
+  });
+};
+
+// scrollback button
+  // Get the button
+  let scrollBackBtn = document.getElementById("scrollBackBtn");
+  let navbarMenuItems = document.getElementById("navBarMenuItems");
+  // When the user scrolls down from the top of the document, show the button
+  window.onscroll = function() {scrollFunction()};
+
+  function scrollFunction() {
+    if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
+      scrollBackBtn.style.display = "block";
+    } else {
+      scrollBackBtn.style.display = "none";
+    }
+  }
+  // When the user clicks on the button, scroll to the top of the document
+  function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+  }
+
+  // Toggle button oppacity, events listeners are inline js
+  function noOpacity() {
+    scrollBackBtn.style.opacity = 1;
+  }
+  function oppacity() {
+    scrollBackBtn.style.opacity = 0.5;
+  }
